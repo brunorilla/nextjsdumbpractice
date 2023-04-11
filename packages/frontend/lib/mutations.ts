e49b6eb8349c3c3b743db3c5e90ea37d3094a6dd
brunorilla <brunorilla9@gmail.com>
2023-04-11 20:03:16 -0300

    refactored dir structure to change everything and set this project as a monorepo

--- packages/frontend/lib/mutations.ts
+++ packages/frontend/lib/mutations.ts
@@ -3,8 +3,7 @@ import {db} from '@/lib/firebase'
 import {NewUser} from "@/types/User"
 import "firebase/firestore";
 import firebase from "firebase/compat";
-import Firestore = firebase.firestore.Firestore;
-
+//import { functions} from '/firebase';
 
 export const createTodo = async (title: string) => {
     try {
@@ -33,6 +32,11 @@ export const deleteTodo = async (id: string) => {
 export async function createNewUser(newUser: NewUser) {
     try {
         const { name, surname, password, email, unit, isDue } = newUser;
+       // const checkUserExists = functions.httpsCallable('checkIfUserExists');
+       // const { data } = await checkUserExists({ email: email });
+       // if (data.exists) {
+        //    throw new Error('A user with this email address already exists');
+       // }
         const response = await addDoc(collection(db, 'user'), { name, surname, password, email, unit, isDue});
 
         console.log("New user created with ID: ", response.id);
