0b4e989fe3f5146e52d44996aba8e1c131880867
brunorilla <brunorilla9@gmail.com>
2023-04-11 20:46:27 -0300

    feat: transferring firebase config to the backend

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,12 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const app = express();
 
-app.listen(5000, ()=> {
-    console.log("Server is running on http://localhost:5000");
-})
+app.listen(3001, () => {
+    console.log("Server is running on http://localhost:3001");
+});
+
+app.get('/api/hello', (req: Request, res: Response) => {
+    console.log("hello");
+    res.json({ message: 'Hello World' });
+});
